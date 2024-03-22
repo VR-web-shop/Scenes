a8af02059fb74893f05d4a9a764f2341cce0a2a0
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-03-22 03:53:45 +0100

    Prepare deployment

--- sdk_deploy.sh
+++ sdk_deploy.sh
@@ -21,26 +21,9 @@ git add .
 git commit -m "Bump version"
 git push origin main
 
-echo ""
-echo -e "\e[34mLooking for the release branch\e[0m"
-if [ -z "$(git branch --list sdk-release)" ]; then
-  echo "Creating the release branch locally"
-  git checkout -b sdk-release
-else
-    echo "Checking out the release branch"
-    git checkout sdk-release
-    
-    echo "Merging the main branch into the release branch"
-    git merge main
-fi
-
-echo ""
-echo -e "\e[34mPushing the release branch to the remote\e[0m"
-git push origin sdk-release
-
-echo ""
-echo -e "\e[34mChecking out the main branch\e[0m"
-git checkout main
+npm ci
+npm run bundle_sdk
+npm publish
 
 echo ""
 echo -e "\e[32mDeployment completed successfully 🚀🚀🚀\e[0m"
