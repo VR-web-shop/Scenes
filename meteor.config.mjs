42ae9f26052a43b2505b9edbffb9ba29268643cf
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-03-26 05:16:33 +0100

    Prepare deployment

--- meteor.config.mjs
+++ meteor.config.mjs
@@ -3,4 +3,9 @@ import meteor from "@vr-web-shop/meteor";
 import Controller from "./src/controllers/api/v1/Controller.js";
 
 const { SERVER_HOST, SERVER_PORT } = process.env
-meteor.BuildAPISDK(`./sdk/sdk.js`, `http://${SERVER_HOST}:${SERVER_PORT}`, Controller)
+meteor.BuildAPISDK(
+    `./sdk/sdk.js`, 
+    `http://${SERVER_HOST}:${SERVER_PORT}`, 
+    Controller,
+    { storage: 'localStorage', key: 'auth' }
+)
